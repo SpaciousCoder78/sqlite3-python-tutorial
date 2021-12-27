97068dc4549900ed290e1c1146ca1066c5d5ca28
hopinggrasshopper <88923986+hopinggrasshopper@users.noreply.github.com>
2021-12-27 19:33:46 +0530

    Create employee.py

---
+++ Tutorial/Adding python objects to database/employee.py
@@ -0,0 +1,8 @@
+class Employee:
+
+    def __init__(self,first,last,pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+#we are creating a class named Employee to make things easier
+#we will be using this class in the program
